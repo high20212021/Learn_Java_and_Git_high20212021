d595b022bf10830a9654dc72744e2d95d87eb450
high20212021 <high20212021@outlook.com>
2023-04-23 23:43:41 +0800

    Update IDE and file

--- doWhile.java
+++ doWhile.java
@@ -4,11 +4,11 @@ public class doWhile {
     public static void main(String [] args){
         Scanner input = new Scanner (System.in);
         //int c = input.nextInt();
-		do{
-            System.out.println("输入你的分数");
-            //int c = input.nextInt();
-        }while( c <= 60 ){
+        System.out.println("输入你的分数");
+        int c = 50;
+        while( c <= 60 ){
             System.out.println("不及格");
+            c = input.nextInt();
         }
         System.out.println("及格");
     }
