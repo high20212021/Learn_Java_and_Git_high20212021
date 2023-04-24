9560c6317b190d8205310c2472da2d178fd84f63
high20212021 <high20212021@outlook.com>
2023-04-25 00:27:12 +0800

    我踏马真脑抽

---
+++ testCollapse1.java
@@ -0,0 +1,10 @@
+public class testCollapse1 {
+    public static void main(String [] args){
+         long d = 9999999999999999L;
+         double e = 0.74;
+         boolean f = true;
+         System.out.println(f);
+         System.out.println( d + e );
+         //我真脑抽
+    }
+}
